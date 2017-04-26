efe48b6ab8ad6410f81fe2a34550d4a37e9e8ee0
mingkuang <mingkuang@live.com>
2017-04-26 11:41:42 +0800

    解决VS1027使用2015平台工具集时，库加载不正确问题

--- _msvcrt.h
+++ _msvcrt.h
@@ -2,14 +2,9 @@
 
 #if defined(NDEBUG)&&defined(_DLL)
 
+#include <crtversion.h>
 
 #ifndef _VCRUNTIME_H
-/*
-__NO_LTL_LIB 宏，可以取消对ltlXXX.lib的引用，取消引用后程序体积可以变得更小，但是必须手动在cpp引用_msvcrt.cpp，否则将编译失败
-ltlXXX.lib是_msvcrt.cpp的静态库
-
-如果希望手动引用_msvcrt.cpp，那么请在顶级.h的最开始 include <_msvcrt.h> 或者 直接在工程定义 __NO_LTL_LIB 宏
-*/
 #define __NO_LTL_LIB
 #endif
 
@@ -21,47 +16,35 @@ ltlXXX.lib是_msvcrt.cpp的静态库
 #pragma comment(linker, "/nodefaultlib:msvcprt.lib")
 
 
-
+#if _VC_CRT_MAJOR_VERSION ==14 && _VC_CRT_MINOR_VERSION==0
+#define __ltlversion "140"
+#elif _VC_CRT_MAJOR_VERSION ==14 && _VC_CRT_MINOR_VERSION== 10
+#define __ltlversion "141"
+#endif
 
 #ifdef _ATL_XP_TARGETING
 //XP模式，此模式很多编译器特性采用事件实现，并将XP不支持的一些函数静态导入
-#if _MSC_VER == 1900
-#pragma comment(lib,"vc140xp.lib")
-
-#ifndef __NO_LTL_LIB
-#pragma comment(lib,"ltl140xp.lib")
-#endif //__NO_LTL_LIB
-
-#elif _MSC_VER == 1910
-#pragma comment(lib,"vc141xp.lib")
-
-#ifndef __NO_LTL_LIB
-#pragma comment(lib,"ltl141xp.lib")
-#endif //__NO_LTL_LIB
-
-#endif
+#define __ltlversionxp "xp"
 #define _ACRTXPIMP extern
-#else
+#else //_ATL_XP_TARGETING else
 //默认模式，此模式编译器新特性将使用Vista新API实现，性能更佳
-#if _MSC_VER == 1900
-#pragma comment(lib,"vc140.lib")
-
-#ifndef __NO_LTL_LIB
-#pragma comment(lib,"ltl140.lib")
-#endif //__NO_LTL_LIB
-
-#elif _MSC_VER == 1910
-
-#pragma comment(lib,"vc141.lib")
-
-#ifndef __NO_LTL_LIB
-#pragma comment(lib,"ltl141.lib")
-#endif
-#endif //__NO_LTL_LIB
-
+#define __ltlversionxp
 #define _ACRTXPIMP __declspec(dllimport)
-#endif
+#endif //_ATL_XP_TARGETING
 
+#ifdef __ltlversion
+
+#pragma comment(lib,"vc" __ltlversion __ltlversionxp ".lib")
+
+#ifndef __NO_LTL_LIB
+/*
+__NO_LTL_LIB 宏，可以取消对ltlXXX.lib的引用，取消引用后程序体积可以变得更小，但是必须手动在cpp include _msvcrt.cpp，否则将编译失败
+ltlXXX.lib是_msvcrt.cpp的静态库 如果你希望手动引用_msvcrt.cpp，那么请在顶级.h的最开始 include <_msvcrt.h> 或者 直接在工程定义 __NO_LTL_LIB 宏
+*/
+#pragma comment(lib,"ltl" __ltlversion __ltlversionxp ".lib")
+#endif //__NO_LTL_LIB
+
+#endif //__ltlversion
 
 //导入Windows Vista 动态库 msvcrt.dll
 #pragma comment(lib,"msvcrt_base.lib")
