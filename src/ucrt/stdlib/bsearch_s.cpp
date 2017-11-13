1f14d68e5be6d7f07c3e219631a039047200dcc7
小鸭子 <vistapengjie@live.cn>
2017-11-13 13:45:45 +0800

    为Windows XP添加bsearch_s静态实现

--- src/ucrt/stdlib/bsearch_s.cpp
+++ src/ucrt/stdlib/bsearch_s.cpp
@@ -5,9 +5,14 @@
 //
 // Defines _bsearch_s(), which performs a binary search over an array.
 //
+
+#ifdef _ATL_XP_TARGETING
+
 #ifdef __USE_CONTEXT
     #error __USE_CONTEXT should be undefined
 #endif
 
 #define __USE_CONTEXT
 #include "bsearch.cpp"
+
+#endif
