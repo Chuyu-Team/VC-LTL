e36edcb2af4d15ca300886634eaab1a0e351cbc6
小鸭子 <mingkuang@live.com>
2017-11-20 12:58:19 +0800

    添加 _DISABLE_DEPRECATE_LTL_MESSAGE 支持，移除某些 VC-LTL的警告（感谢 Renshk）

--- _msvcrt.h
+++ _msvcrt.h
@@ -72,40 +72,38 @@
 #error "vc-ltl 并不支持当前目标平台，请切换目标平台版本至 10240/14393/15063（推荐）。切换目标平台仅仅是切换了Windows SDK/UCRT版本，这并不影响你兼容老版本Windows（包括Windows XP）。"
 #elif _UCRT_VERISON == 10240
 
-#ifndef _USING_V110_SDK71_
+#if !defined _USING_V110_SDK71_ && !defined _DISABLE_DEPRECATE_LTL_MESSAGE
 #pragma message(_ltlfilelen "warning: 10240 ucrt 存在的目的仅用于兼容Windows XP工具集正常编译，而你的程序并未采用XP兼容，强烈建议你迁徙目标平台到Windows 10 16299。")
 #endif
 
 #elif _UCRT_VERISON == 10586
-#error "warning: 10586 ucrt 已经停止支持，请升级到15063 UCRT或者更高。"
+#error "10586 ucrt 已经停止支持，请升级到15063 UCRT或者更高。"
 #elif _UCRT_VERISON == 14393
-#error "warning: 14393 ucrt 已经停止支持，请升级到15063 UCRT或者更高。"
+#error "14393 ucrt 已经停止支持，请升级到15063 UCRT或者更高。"
 #elif _UCRT_VERISON == 15063
+#ifndef _DISABLE_DEPRECATE_LTL_MESSAGE
 #pragma message(_ltlfilelen "warning: 15063 ucrt 即将在下个Windows 10 SDK发布时删除，请尽快迁徙目标平台到Windows 10 16299。")
+#endif
 #elif _UCRT_VERISON == 16299
 //当前使用16229 UCRT
 #else
-#pragma message(_ltlfilelen "warning: 无法识别此版本是URCT，强烈建议升级到最新VC-LTL然后继续。")
+#error "无法识别此版本是URCT，强烈建议升级到最新VC-LTL然后继续。"
 #endif
 
+#ifndef _DISABLE_DEPRECATE_LTL_MESSAGE
+#pragma message(_ltlfilelen "info: 进入ltl普通模式，已准备引用到VC-LTL。定义 _DISABLE_DEPRECATE_LTL_MESSAGE 可关闭信息提示。")
+#endif //_DISABLE_DEPRECATE_LTL_MESSAGE
 
-
-
-#if defined(__NO_LTL_LIB) || defined(__Build_LTL)
-
-#pragma message(_ltlfilelen "info: 进入ltl超越模式，请务必手动引用 _msvcrt.cpp ，否则将编译失败。取消定义 __NO_LTL_LIB 可返回普通模式。")
-
-#else
-
-//__NO_LTL_LIB 宏，可以取消对ltlXXX.lib的引用，取消引用后程序体积可以变得更小，但是必须手动在cpp include _msvcrt.cpp，否则将编译失败
 #ifndef _LIB
+
+#ifdef __NO_LTL_LIB
+#pragma message(_ltlfilelen "warning: 进入ltl超越模式已经弃用，此选项将将被忽略。")
+#endif
+
+//导入ltl补充库
 #pragma comment(lib,"ltl" __ltlversionxp ".lib")
-#pragma message(_ltlfilelen "info: 进入ltl普通模式，已准备对" "ltl" __ltlversionxp ".lib" "的引用。定义 __NO_LTL_LIB  可切换到超越模式。")
-#endif //!_LIB
 
-#endif //__NO_LTL_LIB || __Build_LTL
 
-#ifndef _LIB
 //导入VC编译器新特性
 #pragma comment(lib,"vc.lib")
 //导入UCRT新特性
@@ -121,7 +119,7 @@ __LTL_Initialization用于初始化 LTL_Initialization.cpp 全局构造
 
 你可以定义 _NO__LTL_Initialization 来移除 LTL 初始化
 
-但是当你使用iostrean以及_lock_locales相关功能时将导致程序崩溃
+但是当你使用iostream等功能时将导致程序崩溃
 
 */
 #ifdef _M_IX86
