13a899aca9efdf07193d120e321bc2eab2398f2c
小鸭子 <mingkuang@live.com>
2018-01-18 16:19:51 +0800

    新增__control87_2_downlevel

--- src/_msvcrt.cpp
+++ src/_msvcrt.cpp
@@ -2486,15 +2486,9 @@ extern "C"
 #ifdef _M_IX86
 	extern BOOL __isa_available;
 
-	static __forceinline unsigned int __get_fpsr_sse2()
-	{
-		if (__isa_available < 1)
-			return 0;
-		else
-			return _mm_getcsr();
-	}
+	extern unsigned int __cdecl __get_fpsr_sse2();
 
-	static __forceinline unsigned int __statusfp_sse2()
+	static __forceinline unsigned int __cdecl __statusfp_sse2()
 	{
 		auto fpsr_sse2 = __get_fpsr_sse2();
 
@@ -2566,6 +2560,249 @@ extern "C"
 	}
 
 	_LCRT_DEFINE_IAT_SYMBOL(_statusfp2_downlevel);
+
+
+	static unsigned short __cdecl _hw_cw(unsigned int  _NewValue)
+	{
+		unsigned int result = 0;
+
+		if (_NewValue & 0x10)
+			result = 0x1;
+
+		if (_NewValue & 0x8)
+			result |= 0x4;
+
+		if (_NewValue & 0x4)
+			result |= 0x8;
+
+		if (_NewValue & 0x2)
+			result |= 0x10;
+
+		if (_NewValue & 0x1)
+			result |= 0x20;
+
+		if (_NewValue & 0x80000)
+			result |= 0x2;
+
+		if (_NewValue & 0x100)
+			result |= 0x400;
+
+		if (_NewValue & 0x200)
+			result |= 0x800;
+
+		if ((_NewValue & 0x30000)== 0x10000)
+			result |= 0x200;
+		else
+			result |= 0x300;
+
+		if (_NewValue & 0x40000)
+			result |= 0x1000;
+
+		return result;
+	}
+
+	static unsigned int __cdecl __hw_cw_sse2(unsigned int Sse2ControlWord)
+	{
+		unsigned int result = 0;
+
+		if (Sse2ControlWord & 0x10)
+			result = 0x80;
+		if (Sse2ControlWord & 0x08)
+			result |= 0x200u;
+		if (Sse2ControlWord & 0x04)
+			result |= 0x400u;
+		if (Sse2ControlWord & 0x02)
+			result |= 0x800u;
+		if (Sse2ControlWord & 0x01)
+			result |= 0x1000u;
+		if (Sse2ControlWord & 0x80000)
+			result |= 0x100u;
+
+		if (Sse2ControlWord & 0x100)
+			result |= 0x2000u;
+
+		if (Sse2ControlWord & 0x200)
+			result |= 0x4000u;
+
+		
+		switch (Sse2ControlWord & 0x3000000)
+		{
+		case 0x1000000:
+			result |= 0x8040u;
+			break;
+		case 0x2000000:
+			result |= 0x40u;
+			break;
+		case 0x3000000:
+			result |= 0x8000u;
+			break;
+		default:
+			break;
+		}
+
+		return result;
+	}
+
+	static unsigned int __fastcall FpuStatusToX86ControlWord(unsigned short FpuStatus)
+	{
+		unsigned int CurrentX86ControlWord = 0;
+
+		if (FpuStatus & 0x1)
+			CurrentX86ControlWord |= 0x10;
+
+		if (FpuStatus & 0x2)
+			CurrentX86ControlWord |= 0x80000;
+
+		if (FpuStatus & 0x4)
+			CurrentX86ControlWord |= 0x8;
+
+		if (FpuStatus & 0x8)
+			CurrentX86ControlWord |= 0x4;
+
+		if (FpuStatus & 0x10)
+			CurrentX86ControlWord |= 0x2;
+
+		if (FpuStatus & 0x20)
+			CurrentX86ControlWord |= 0x1;
+
+
+		if (FpuStatus & 0x400)
+			CurrentX86ControlWord |= 0x100;
+
+		if (FpuStatus & 0x800)
+			CurrentX86ControlWord |= 0x200;
+
+		if ((FpuStatus & 0x300) == 0x200)
+		{
+			CurrentX86ControlWord |= 0x10000;
+		}
+		else
+		{
+			CurrentX86ControlWord |= 0x20000;
+		}
+
+		if (FpuStatus & 0x1000)
+		{
+			CurrentX86ControlWord |= 0x40000;
+		}
+
+		return CurrentX86ControlWord;
+	}
+
+	static unsigned int __fastcall Sse2StatusToSse2ControlWord(unsigned int Sse2Status)
+	{
+		unsigned int Sse2ControlWord = 0;
+
+		if (Sse2Status & 0x8000)
+			Sse2ControlWord = 0x10;
+
+		if (Sse2Status & 0x200)
+			Sse2ControlWord |= 0x8;
+
+		if (Sse2Status & 0x400)
+			Sse2ControlWord |= 0x4;
+
+		if (Sse2Status & 0x800)
+			Sse2ControlWord |= 0x2;
+
+		if (Sse2Status & 0x1000)
+			Sse2ControlWord |= 0x1;
+
+		if (Sse2Status & 0x100)
+			Sse2ControlWord |= 0x80000;
+
+		if (Sse2Status & 0x2000)
+			Sse2ControlWord |= 0x100;
+
+		if (Sse2Status & 0x4000)
+			Sse2ControlWord |= 0x200;
+
+
+		switch (Sse2Status & 0x8040)
+		{
+		case 0x40:
+			Sse2ControlWord |= 0x2000000;
+			break;
+		case 0x8000:
+			Sse2ControlWord |= 0x3000000;
+			break;
+		case 0x8040:
+			Sse2ControlWord |= 0x1000000;
+			break;
+		default:
+			break;
+		}
+
+		return Sse2ControlWord;
+	}
+
+	extern void __cdecl __set_fpsr_sse2(unsigned int);
+
+	int __cdecl __control87_2_downlevel(
+		_In_      unsigned int  _NewValue,
+		_In_      unsigned int  _Mask,
+		_Out_opt_ unsigned int* _X86ControlWord,
+		_Out_opt_ unsigned int* _Sse2ControlWord
+		)
+	{
+		_NewValue &= _Mask;
+
+		if (_X86ControlWord)
+		{
+			unsigned short FpuStatus;
+
+			__asm { fstcw FpuStatus };
+
+			unsigned int CurrentX86ControlWord = FpuStatusToX86ControlWord(FpuStatus);
+
+
+			auto NewX86ControlWord = (_NewValue) | (CurrentX86ControlWord&~_Mask);
+
+
+			if (NewX86ControlWord != CurrentX86ControlWord)
+			{
+				FpuStatus = _hw_cw(NewX86ControlWord);
+
+				__asm
+				{
+					fldcw FpuStatus
+					fstcw FpuStatus
+				}
+
+				NewX86ControlWord = FpuStatusToX86ControlWord(FpuStatus);
+			}
+
+			*_X86ControlWord = NewX86ControlWord;
+		}
+
+
+		if (_Sse2ControlWord)
+		{
+			if (__isa_available < 1)
+			{
+				*_Sse2ControlWord = 0;
+			}
+			else
+			{
+				auto CurrentSse2ControlWord = Sse2StatusToSse2ControlWord(_mm_getcsr());
+
+				auto NewSse2ControlWord = (_NewValue & 0x308031F) | (CurrentSse2ControlWord&~(_Mask & 0x308031F));
+
+				if (NewSse2ControlWord != CurrentSse2ControlWord)
+				{
+					__set_fpsr_sse2(__hw_cw_sse2(NewSse2ControlWord));
+
+					CurrentSse2ControlWord= Sse2StatusToSse2ControlWord(_mm_getcsr());
+				}
+
+				*_Sse2ControlWord = CurrentSse2ControlWord;
+			}
+		}
+
+		return 1;
+	}
+
+	_LCRT_DEFINE_IAT_SYMBOL(__control87_2_downlevel);
 
 #endif
 }
