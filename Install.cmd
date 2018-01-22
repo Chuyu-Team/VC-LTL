6b8e419eea1575b6923e670c81d3256e503598a2
MouriNaruto <Mouri_Naruto@outlook.com>
2018-01-22 18:19:12 +0800

    改善安装器

--- Install.cmd
+++ Install.cmd
@@ -1,4 +1,66 @@
+@echo off
+setlocal
+set IsTextMode=No
 
+echo VC-LTL Installer [Version 0.1.0]
+echo (c) Chuyu Team. All rights reserved.
+echo.
+
+if /i "%1"=="" goto TextMode
+if /i "%1"=="/?" goto Help
+if /i "%1"=="/I" goto Install
+if /i "%1"=="/U" goto UnInstall
+
+echo Invaild Command Parameter.
+echo.
+goto Help
+
+:Install
+echo.
 reg ADD HKCU\Code\VC-LTL /v Root /t REG_SZ /d "%~dp0\" /f
+echo.
+goto ExitScript
 
-pause
+:UnInstall
+echo.
+reg delete HKCU\Code\VC-LTL /f
+echo.
+goto ExitScript
+
+:Help
+echo.Manages VC-LTL information in the current user registry.
+echo.
+echo.Install [Options]
+echo.
+echo.  /I    Add VC-LTL information to the current user registry.
+echo.  /U    Remove VC-LTL information to the current user registry.
+echo.  /?    Show this content.
+goto ExitScript
+
+:TextMode
+set IsTextMode=Yes
+echo.Welcome to use VC-LTL Installer! If you want to invoke this script 
+echo.silently, please use /? parameter to read help.
+reg query HKCU\Code\VC-LTL >nul 2>nul && goto TextUnInstall || goto TextInstall
+
+:TextInstall
+echo. 
+set /p Answer=VC-LTL does not exist, do you want to install it (Y/N)?
+if /i "%Answer%"=="Y" goto Install
+echo.
+echo.Operation canceled.
+echo.
+goto ExitScript
+
+:TextUnInstall
+echo. 
+set /p Answer=VC-LTL appears to be installed, do you want to uninstall it (Y/N)?
+if /i "%Answer%"=="Y" goto UnInstall
+echo.
+echo.Operation canceled.
+echo.
+goto ExitScript
+
+:ExitScript
+if /i "%IsTextMode%"=="Yes" pause
+@echo on
