030a3e9049f1ec2fc2e6247802902690f1f5f980
小鸭子 <mingkuang@live.com>
2018-04-22 20:39:43 +0800

    添加cmake helper，方便cmake用户使用VC-LTL（感谢  Sandro）

---
+++ VC-LTL helper for cmake.cmake
@@ -0,0 +1,71 @@
+﻿cmake_minimum_required(VERSION 3.5.2)
+
+#############################################################################################################################
+#
+#   VC-LTL自动化加载配置，建议你将此文件单独复制到你的工程再使用，该文件能自动识别当前环境是否存在VC-LTL，并且自动应用。
+#   使用方法：
+#   在“CMakeLists.txt” 添加 include("VC-LTL helper for cmake.cmake") 即可。
+# VC-LTL默认搜索顺序
+#
+# 1：当前CMake根目录，即${CMAKE_CURRENT_SOURCE_DIR}
+# 2：当前项目根目录，即${PROJECT_SOURCE_DIR}
+# 4：当前CMake父目录，即${CMAKE_CURRENT_SOURCE_DIR}/..
+# 5：当前项目根目录，即${PROJECT_SOURCE_DIR}/..
+# 6：注册表HKEY_CURRENT_USER\Code\VC-LTL@Root
+#
+# 把VC-LTL放在其中一个位置即可，VC-LTL就能被自动引用。
+# 
+# 如果你对默认搜索顺序不满，你可以修改此文件。你也可以直接指定${VC_LTL_Root}宏更加任性的去加载VC-LTL。
+#
+#############################################################################################################################
+
+
+
+
+
+
+#取消下方注释可以开启XP兼容模式，默认才用Vista兼容模式。
+#set(SupportWinXP "true")
+
+#取消下方注释可以开启VC-LTL轻量模式，轻量模式更加注重体积控制，但是CRT规范将会维持在VS2008。如果你需要高度兼容微软UCRT，那么请不要打开此选项！！！
+#set(DisableAdvancedSupport "true")
+
+
+if(NOT VC_LTL_Root)
+	find_file(FOUND_FILE _msvcrt.h ${CMAKE_CURRENT_SOURCE_DIR}/VC-LTL)
+	if(NOT ${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+		set(VC_LTL_Root ${CMAKE_CURRENT_SOURCE_DIR}/VC-LTL)
+	endif()
+endif()
+
+if(NOT VC_LTL_Root)
+    find_file(FOUND_FILE _msvcrt.h ${PROJECT_SOURCE_DIR}/VC-LTL)
+    if(NOT ${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+        set(VC_LTL_Root ${PROJECT_SOURCE_DIR}/VC-LTL)
+    endif()
+endif()
+
+if(NOT VC_LTL_Root)
+    find_file(FOUND_FILE _msvcrt.h ${CMAKE_CURRENT_SOURCE_DIR}/../VC-LTL)
+    if(NOT ${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+        set(VC_LTL_Root ${CMAKE_CURRENT_SOURCE_DIR}/../VC-LTL)
+    endif()
+endif()
+
+if(NOT VC_LTL_Root)
+    find_file(FOUND_FILE _msvcrt.h ${PROJECT_SOURCE_DIR}/../VC-LTL)
+    if(NOT ${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+        set(VC_LTL_Root ${PROJECT_SOURCE_DIR}/../VC-LTL)
+    endif()
+endif()
+
+if(NOT VC_LTL_Root)
+    GET_FILENAME_COMPONENT(FOUND_FILE "[HKEY_CURRENT_USER\\Code\\VC-LTL;Root]" ABSOLUTE)
+    if (NOT ${FOUND_FILE} STREQUAL "registry")
+        set(VC_LTL_Root ${FOUND_FILE})
+    endif()
+endif()
+
+if(VC_LTL_Root)
+    include("${VC_LTL_Root}/config/config.cmake")
+endif()
---
+++ config/config.cmake
@@ -0,0 +1,175 @@
+﻿cmake_minimum_required(VERSION 3.5.2)
+
+
+if(NOT SupportWinXP)
+	set(SupportWinXP "false")
+endif()
+
+if(NOT DisableAdvancedSupport)
+	set(DisableAdvancedSupport "false")
+endif()
+
+if(NOT SupportLTL)
+	set(SupportLTL "true")
+endif()
+
+
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+	message("VC-LTL not load in Debug!!!")
+	set(SupportLTL "false")
+endif()
+
+
+if(${SupportLTL} STREQUAL "true")
+
+	include(CheckSymbolExists)
+
+	check_symbol_exists("_M_AMD64" "" _M_AMD64)
+
+	if(_M_AMD64)
+		set(PlatformShortName "x64")
+	else()
+		check_symbol_exists("_M_IX86" "" _M_IX86)
+		if(_M_IX86)
+			set(PlatformShortName "x86")
+		else()
+			message("VC-LTL not load, Unknown Platform!!!")
+			set(SupportLTL "false")
+		endif()
+	endif()
+endif()
+
+
+if(${SupportLTL} STREQUAL "true")
+	#获取VC版本号
+	if(NOT VC-LTLUsedToolsVersion)
+		if(${MSVC_VERSION} EQUAL 1900)
+			set(__DefaultVCToolsVersion "14.0.23918")
+
+			#VC 2015只能自己读取注册表解决
+			GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{B0791F3A-6A88-3650-AECF-8AFBE227EC53};DisplayVersion]" NAME)
+			if (${VCToolsVersion} STREQUAL "registry")
+				GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{B0791F3A-6A88-3650-AECF-8AFBE227EC53};DisplayVersion]" NAME)
+			endif()
+
+			if (${VCToolsVersion} STREQUAL "registry")
+				GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{4B1849F2-3D49-325F-B997-4AD0BF5B8A09};DisplayVersion]" NAME)
+			endif()
+			if (${VCToolsVersion} STREQUAL "registry")
+				GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{4B1849F2-3D49-325F-B997-4AD0BF5B8A09};DisplayVersion]" NAME)
+			endif()
+
+			if (${VCToolsVersion} STREQUAL "registry")
+				GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{729FD64C-2AE0-3E25-83A8-A93520DCDE7A};DisplayVersion]" NAME)
+			endif()
+			if (${VCToolsVersion} STREQUAL "registry")
+				GET_FILENAME_COMPONENT(VCToolsVersion "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{729FD64C-2AE0-3E25-83A8-A93520DCDE7A};DisplayVersion]" NAME)
+			endif()
+
+			if (NOT ${VCToolsVersion} STREQUAL "registry")
+				set(VC-LTLUsedToolsVersion ${VCToolsVersion})
+			endif()
+
+		elseif(${MSVC_VERSION} LESS 1920)
+			#VC2017
+			set(__DefaultVCToolsVersion "14.10.25017")
+
+			if(DEFINED ENV{VCToolsVersion})
+				set(VC-LTLUsedToolsVersion $ENV{VCToolsVersion})
+			endif()
+
+		else()
+			message("VC-LTL not load, Unknown MSVC_VERSION=" ${MSVC_VERSION})
+			set(SupportLTL "false")
+		endif()
+
+		if(${SupportLTL} STREQUAL "true")
+			if(NOT VC-LTLUsedToolsVersion)
+				set(VC-LTLUsedToolsVersion ${__DefaultVCToolsVersion})
+			else()
+				find_file(FOUND_FILE ${VC-LTLUsedToolsVersion} ${VC_LTL_Root}/VC)
+				if(${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+					set(VC-LTLUsedToolsVersion ${__DefaultVCToolsVersion})
+				endif()
+
+			endif()
+		endif()
+
+	endif()
+endif()
+
+
+if(${SupportLTL} STREQUAL "true")
+
+	#获取UCRT版本，如果当前环境变量UCRTVersion说对应的版本VC-LTL支持，那么使用该版本，否则使用10.0.10240.0
+   if(NOT VC-LTLTargetUniversalCRTVersion)
+	   if(DEFINED ENV{UCRTVersion})
+	   		find_file(FOUND_FILE $ENV{UCRTVersion} ${VC_LTL_Root}/ucrt)
+			if(NOT ${FOUND_FILE} STREQUAL "FOUND_FILE-NOTFOUND")
+				set(VC-LTLTargetUniversalCRTVersion $ENV{UCRTVersion})
+			endif()
+		endif()
+
+		if(NOT VC-LTLTargetUniversalCRTVersion)
+			#设置默认UCRT版本号
+			set(VC-LTLTargetUniversalCRTVersion "10.0.10240.0")
+		endif()
+	endif()
+
+
+	#设置OsPlatformName
+	if(${SupportWinXP} STREQUAL "true")
+		set(OsPlatformName "WinXP")
+	else()
+		set(OsPlatformName "Vista")
+	endif()
+
+	#设置LTL_Mode
+	if(${DisableAdvancedSupport} STREQUAL "true")
+		set(LTL_Mode "Light")
+	else()
+		set(LTL_Mode "Advanced")
+	endif()
+
+	#打印VC-LTL基本信息
+	message("################################################################################################################################")
+	message("#                                                                                                                              #")
+	message("#                                                                                                                              #")
+	message("# VC-LTL Path :" ${VC_LTL_Root})
+	message("# VC-LTL Tools Version :" ${VC-LTLUsedToolsVersion})
+	message("# VC-LTL UCRT Version :" ${VC-LTLTargetUniversalCRTVersion})
+	message("# Platform :" ${PlatformShortName})
+	message("# Using VC-LTL " ${OsPlatformName} " " ${LTL_Mode} " Mode")
+	message("#                                                                                                                              #")
+	message("#                                                                                                                              #")
+	message("################################################################################################################################")
+
+    set(VC_LTL_Include ${VC_LTL_Root}/config/${OsPlatformName};${VC_LTL_Root}/VC/${VC-LTLUsedToolsVersion}/include;${VC_LTL_Root}/VC/${VC-LTLUsedToolsVersion}/atlmfc/include;${VC_LTL_Root}/ucrt/${VC-LTLTargetUniversalCRTVersion})
+    set(VC_LTL_Library ${VC_LTL_Root}/${PlatformShortName};${VC_LTL_Root}/${PlatformShortName}/${OsPlatformName}/${LTL_Mode};${VC_LTL_Root}/VC/${VC-LTLUsedToolsVersion}/lib/${PlatformShortName};${VC_LTL_Root}/VC/${VC-LTLUsedToolsVersion}/lib/${PlatformShortName}/${OsPlatformName};${VC_LTL_Root}/ucrt/${VC-LTLTargetUniversalCRTVersion}/lib/${PlatformShortName})
+
+	#message("INCLUDE " $ENV{INCLUDE})
+	#message("LIB " $ENV{LIB})
+
+	#set( ENV{INCLUDE} ${VC_LTL_Include};$ENV{INCLUDE})
+	#set( ENV{LIB} ${VC_LTL_Library};$ENV{LIB})
+
+
+	#message("INCLUDE " $ENV{INCLUDE})
+	#message("LIB " $ENV{LIB})
+
+    include_directories(BEFORE SYSTEM ${VC_LTL_Include})
+    link_directories(${VC_LTL_Library})
+
+	#message("INCLUDE " $ENV{INCLUDE})
+	#message("LIB " $ENV{LIB})
+
+    if(${SupportWinXP} STREQUAL "true")
+       if(${PlatformShortName} STREQUAL "x86")
+			set(CMAKE_CREATE_WIN32_EXE "/subsystem:windows,5.01")
+			set(CMAKE_CREATE_CONSOLE_EXE "/subsystem:console,5.01")
+		else()#x64
+			set(CMAKE_CREATE_WIN32_EXE "/subsystem:windows,5.02")
+			set(CMAKE_CREATE_CONSOLE_EXE "/subsystem:console,5.02")
+		endif()
+    endif()
+endif()
