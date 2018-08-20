85f05a6c26abc53bfb2731164625dc607359b7f1
mingkuang <mingkuang@live.com>
2018-08-20 18:08:59 +0800

    NMake脚本添加%cd%环境变量支持。

--- VC-LTL helper for nmake.cmd
+++ VC-LTL helper for nmake.cmd
@@ -4,13 +4,18 @@
 ::  VC-LTL自动化加载配置，建议你将此文件单独复制到你的工程再使用，该文件能自动识别当前环境是否存在VC-LTL，并且自动应用。
 ::
 ::  使用方法：
-::    1：启动 vcvars32.bat/vcvars64.bat 然后执行此脚本，运行完成后将自动修改include以及lib环境变量，方便nmake以及纯cl用户引用VC-LTL。
-::    2：务必保证所有依赖的静态库也均用VC-LTL重新编译。
+::    1. 启动 vcvars32.bat/vcvars64.bat 然后执行此脚本，运行完成后将自动修改include以及lib环境变量，方便nmake以及纯cl用户引用VC-LTL。
+::    2. 务必保证所有依赖的静态库也均用VC-LTL重新编译。
 ::
 ::  VC-LTL默认搜索顺序
-::	  1：“VC-LTL helper for nmake.cmd”所在根目录，即 %~dp0
-::    2：“VC-LTL helper for nmake.cmd”所在父目录，即 %~dp0..
-::    3：注册表HKEY_CURRENT_USER\Code\VC-LTL@Root
+::	  1. “VC-LTL helper for nmake.cmd”所在根目录，即 %~dp0
+::	  2. “VC-LTL helper for nmake.cmd”所在根目录下的VC-LTL目录，即 %~dp0VC-LTL
+::	  3. “VC-LTL helper for nmake.cmd”所在父目录，即 %~dp0..
+::	  3. “VC-LTL helper for nmake.cmd”所在父目录下的VC-LTL目录，即 %~dp0..\VC-LTL
+::    4. 当前目录，即 %cd%
+::    5. 当前目录下的VC-LTL目录，即 %cd%\VC-LTL
+::    6. 当前目录的父目录下的VC-LTL目录，即 %cd%\..\VC-LTL
+::    7. 注册表HKEY_CURRENT_USER\Code\VC-LTL@Root
 ::
 ::  把VC-LTL放在其中一个位置即可，VC-LTL就能被自动引用。
 ::
@@ -41,8 +46,24 @@ if "%VC_LTL_Root%" NEQ "" goto LoadConfig
 ::脚本文件根目录存在VC-LTL？
 if exist "%~dp0_msvcrt.h" set VC_LTL_Root=%~dp0&& goto LoadConfig
 
+::脚本文件根目录下存在VC-LTL？
+if exist "%~dp0VC-LTL\_msvcrt.h" set VC_LTL_Root=%~dp0VC-LTL&& goto LoadConfig
+
 ::脚本文件父目录存在VC-LTL？
 if exist "%~dp0..\_msvcrt.h" set VC_LTL_Root=%~dp0..&& goto LoadConfig
+
+::脚本文件父目录存在VC-LTL？
+if exist "%~dp0..\VC-LTL\_msvcrt.h" set VC_LTL_Root=%~dp0..\VC-LTL&& goto LoadConfig
+
+::当前根目录就是VC-LTL？
+if exist "%cd%\_msvcrt.h" set VC_LTL_Root=%cd%&& goto LoadConfig
+
+::当前根目录存在VC-LTL？
+if exist "%cd%\VC-LTL\_msvcrt.h" set VC_LTL_Root=%cd%\VC-LTL&& goto LoadConfig
+
+::当前父目录存在VC-LTL？
+if exist "%cd%\..\VC-LTL\_msvcrt.h" set VC_LTL_Root=%cd%\..\VC-LTL&& goto LoadConfig
+
 
 ::读取注册表 HKCU\Code\VC-LTL@Root
 for /f "tokens=1,2*" %%i in ('reg query "HKCU\Code\VC-LTL" /v Root ') do set VC_LTL_Root=%%k
