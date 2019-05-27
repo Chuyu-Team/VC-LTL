c086ff839cbcfd42178ad483141415679d93ff40
mingkuang <mingkuang@live.com>
2019-05-27 12:58:07 +0800

    解决x64、arm64、arm缺失__p__commode符号问题。

--- src/ucrt/stdio/ncommode.cpp
+++ src/ucrt/stdio/ncommode.cpp
@@ -5,17 +5,19 @@
 //
 // Defines _commode, which sets the default file commit mode to nocommit.
 //
-//#include <corecrt_internal_stdio.h>
+#include <corecrt_internal_stdio.h>
 
 
 
 // Set default file commit mode to nocommit
-extern "C" __declspec(dllimport) extern int _commode;
-
+namespace LTL
+{
+	extern "C" __declspec(dllimport) extern int _commode;
+}
 
 #if defined _M_AMD64 || defined _M_ARM64 || defined _M_ARM
 extern "C" int* __cdecl __p__commode()
 {
-    return &_commode;
+    return &LTL::_commode;
 }
 #endif
