8cecaf8d32f5c1e8804cf38304036a076097899f
mingkuang <mingkuang@live.com>
2019-12-21 14:12:21 +0800

    添加LTL版本号宏，方便开发者进行调节判断。

---
+++ config/LTL_Version.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#ifndef __LTL_Verson_Include
+#define __LTL_Verson_Include
+
+#define LTLVersion1 4
+#define LTLVersion2 0
+#define LTLVersion3 3
+#define LTLVersion4 8
+
+#endif
+
